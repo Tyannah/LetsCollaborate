de13247fd326a1a069faa4a4c0ed8d00cdb38b7f
lajuPDBE <arnlaju@gmail.com>
2021-03-26 13:04:03 +0300

    Code to switch the relay on and off

---
+++ Relay_code/Relay_code.ino
@@ -0,0 +1,13 @@
+#define relay 8
+void setup() {
+  // put your setup code here, to run once:
+pinMode(relay,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(relay,HIGH);
+delay(2000);
+digitalWrite(relay,LOW);
+delay(2000);
+}
